f17ac95cbbadf92c06bd24f4d1fb2de8ff7d359c
kf <kevtfung@gmail.com>
2024-04-03 17:59:18 -0700

    update

--- docs/.vuepress/.temp/pages/blog/2024-04-30_Opus22_Decks.html.vue
+++ docs/.vuepress/.temp/pages/blog/2024-04-30_Opus22_Decks.html.vue
@@ -1,62 +1,121 @@
 <template><div><TagLinks /><!-- http://localhost:8080/blog/2024-04-30_Opus22_Decks.html -->
 <h1 id="opus-22-decks" tabindex="-1"><a class="header-anchor" href="#opus-22-decks" aria-hidden="true">#</a> Opus 22 Decks</h1>
-<h2 id="wol-7" tabindex="-1"><a class="header-anchor" href="#wol-7" aria-hidden="true">#</a> Wol 7</h2>
+<h2 id="tier-list" tabindex="-1"><a class="header-anchor" href="#tier-list" aria-hidden="true">#</a> Tier List</h2>
+<h4 id="tier-1" tabindex="-1"><a class="header-anchor" href="#tier-1" aria-hidden="true">#</a> Tier 1</h4>
+<h4 id="tier-2" tabindex="-1"><a class="header-anchor" href="#tier-2" aria-hidden="true">#</a> Tier 2</h4>
+<h4 id="tier-3" tabindex="-1"><a class="header-anchor" href="#tier-3" aria-hidden="true">#</a> Tier 3</h4>
+<h2 id="recent-events" tabindex="-1"><a class="header-anchor" href="#recent-events" aria-hidden="true">#</a> Recent Events</h2>
+<p><a href="https://ffdecks.com/tournament/2nd-shanghai-mengshu-cup/5090503440728064">(3/8) - 2nd-shanghai-mengshu-cup</a></p>
+<p><a href="https://ffdecks.com/tournament/fifth-annual-meijinsen/6255390711873536">(2/23) - fifth-annual-meijinsen</a></p>
+<p><a href="https://ffdecks.com/tournament/king-kong-ff-vii-rebirth-3v3-team-tournament/6086077010935808">(2/17) - king-kong-ff-vii-rebirth-3v3-team-tournament</a></p>
+<p><a href="https://ffdecks.com/tournament/winter-cup-north-carolina-day-2-singles/6301328356671488">(1/20) - winter-cup-north-carolina-day-2-singles</a></p>
+<p><a href="https://ffdecks.com/tournament/fftcg-world-championships-2023-top-6/6197759347523584">(12/1/23) - fftcg-world-championships-2023-top-6</a></p>
+<h2 id="tribals" tabindex="-1"><a class="header-anchor" href="#tribals" aria-hidden="true">#</a> Tribals</h2>
+<h3 id="wol-7" tabindex="-1"><a class="header-anchor" href="#wol-7" aria-hidden="true">#</a> Wol 7</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/21-121L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/14-023L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/19-102L_eg.jpg" style="max-width:20%; height:auto;">
-<h2 id="fire-water-warriors" tabindex="-1"><a class="header-anchor" href="#fire-water-warriors" aria-hidden="true">#</a> Fire/Water Warriors</h2>
+<p><a href="https://ffdecks.com/deck/5182233607405568">[FFDecks]</a></p>
+<h3 id="warriors" tabindex="-1"><a class="header-anchor" href="#warriors" aria-hidden="true">#</a> Warriors</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/21-010H_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/18-130L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/13-125R_eg.jpg" style="max-width:20%; height:auto;">
-<h2 id="refia-4-color-warrior-of-light" tabindex="-1"><a class="header-anchor" href="#refia-4-color-warrior-of-light" aria-hidden="true">#</a> Refia 4 Color Warrior of Light</h2>
+<p><a href="">[FFDecks]</a></p>
+<h3 id="knights" tabindex="-1"><a class="header-anchor" href="#knights" aria-hidden="true">#</a> Knights</h3>
+<img src="https://storage.googleapis.com/ffdecks-card-images/12-103H_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/14-007L_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/18-015R_eg.jpg" style="max-width:20%; height:auto;">
+<p><a href="https://ffdecks.com/deck/6210143231410176">[FFDecks]</a></p>
+<h3 id="warriors-of-light" tabindex="-1"><a class="header-anchor" href="#warriors-of-light" aria-hidden="true">#</a> Warriors of Light</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/19-102L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/19-128L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/18-012L_eg.jpg" style="max-width:20%; height:auto;">
 <p><a href="https://ffdecks.com/deck/6220250161872896">[FFDecks]</a></p>
-<h2 id="earth-water-chaos-ark" tabindex="-1"><a class="header-anchor" href="#earth-water-chaos-ark" aria-hidden="true">#</a> Earth/Water Chaos Ark</h2>
-<img src="https://storage.googleapis.com/ffdecks-card-images/19-119L_eg.jpg" style="max-width:20%; height:auto;">
-<img src="https://storage.googleapis.com/ffdecks-card-images/16-129L_eg.jpg" style="max-width:20%; height:auto;">
-<img src="https://storage.googleapis.com/ffdecks-card-images/19-105H_eg.jpg" style="max-width:20%; height:auto;">
-<h2 id="ice-water-chaos-ark" tabindex="-1"><a class="header-anchor" href="#ice-water-chaos-ark" aria-hidden="true">#</a> Ice/Water Chaos Ark</h2>
-<img src="https://storage.googleapis.com/ffdecks-card-images/21-027L_eg.jpg" style="max-width:20%; height:auto;">
-<img src="https://storage.googleapis.com/ffdecks-card-images/17-138S_eg.jpg" style="max-width:20%; height:auto;">
-<img src="https://storage.googleapis.com/ffdecks-card-images/21-122H_eg.jpg" style="max-width:20%; height:auto;">
-<h2 id="_8" tabindex="-1"><a class="header-anchor" href="#_8" aria-hidden="true">#</a> 8</h2>
+<h3 id="scions" tabindex="-1"><a class="header-anchor" href="#scions" aria-hidden="true">#</a> Scions</h3>
+<img src="https://storage.googleapis.com/ffdecks-card-images/16-100L_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/20-088L_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/20-086H_eg.jpg" style="max-width:20%; height:auto;">
+<p><a href="">[FFDecks]</a></p>
+<h2 id="categories" tabindex="-1"><a class="header-anchor" href="#categories" aria-hidden="true">#</a> Categories</h2>
+<h3 id="vi" tabindex="-1"><a class="header-anchor" href="#vi" aria-hidden="true">#</a> VI</h3>
+<img src="https://storage.googleapis.com/ffdecks-card-images/15-042R_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/17-002L_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/17-017H_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/20-007L_eg.jpg" style="max-width:20%; height:auto;">
+<p><a href="">[FFDecks]</a></p>
+<h3 id="viii" tabindex="-1"><a class="header-anchor" href="#viii" aria-hidden="true">#</a> VIII</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/22-079L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/22-031H_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/21-081L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/22-010L_eg.jpg" style="max-width:20%; height:auto;">
-<h2 id="_13" tabindex="-1"><a class="header-anchor" href="#_13" aria-hidden="true">#</a> 13</h2>
+<p><a href="">[FFDecks]</a></p>
+<h3 id="xiii" tabindex="-1"><a class="header-anchor" href="#xiii" aria-hidden="true">#</a> XIII</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/19-138S_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/materiahunter-prod.appspot.com/images/cards/PR/PR-146_19-129S_NF_FA.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/19-137S_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/19-130S_eg.jpg" style="max-width:20%; height:auto;">
-<h2 id="mono-earth" tabindex="-1"><a class="header-anchor" href="#mono-earth" aria-hidden="true">#</a> Mono Earth</h2>
+<p><a href="https://ffdecks.com/deck/6227716375314432">[FFDecks]</a></p>
+<h3 id="xiv" tabindex="-1"><a class="header-anchor" href="#xiv" aria-hidden="true">#</a> XIV</h3>
+<img src="https://storage.googleapis.com/ffdecks-card-images/17-016L_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/19-124L_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/14-015R_eg.jpg" style="max-width:20%; height:auto;">
+<p><a href="https://ffdecks.com/deck/6275752363294720">[FFDecks]</a></p>
+<h2 id="dual-elementals" tabindex="-1"><a class="header-anchor" href="#dual-elementals" aria-hidden="true">#</a> Dual Elementals</h2>
+<h3 id="ice-water" tabindex="-1"><a class="header-anchor" href="#ice-water" aria-hidden="true">#</a> Ice/Water</h3>
+<img src="https://storage.googleapis.com/ffdecks-card-images/21-027L_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/17-138S_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/21-122H_eg.jpg" style="max-width:20%; height:auto;">
+<p><a href="https://ffdecks.com/deck/6427954394431488">[FFDecks]</a></p>
+<h3 id="wind-lightning" tabindex="-1"><a class="header-anchor" href="#wind-lightning" aria-hidden="true">#</a> Wind/Lightning</h3>
+<img src="https://storage.googleapis.com/ffdecks-card-images/19-114L_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/17-096H_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/16-135S_eg.jpg" style="max-width:20%; height:auto;">
+<p><a href="https://ffdecks.com/deck/6305146179944448">[FFDecks]</a></p>
+<h3 id="wind-water" tabindex="-1"><a class="header-anchor" href="#wind-water" aria-hidden="true">#</a> Wind/Water</h3>
+<img src="https://storage.googleapis.com/ffdecks-card-images/11-124H_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/21-133S_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/19-118L_eg.jpg" style="max-width:20%; height:auto;">
+<p><a href="https://ffdecks.com/deck/6275657387474944">[FFDecks]</a></p>
+<h3 id="earth-water" tabindex="-1"><a class="header-anchor" href="#earth-water" aria-hidden="true">#</a> Earth/Water</h3>
+<img src="https://storage.googleapis.com/ffdecks-card-images/19-119L_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/16-129L_eg.jpg" style="max-width:20%; height:auto;">
+<img src="https://storage.googleapis.com/ffdecks-card-images/19-105H_eg.jpg" style="max-width:20%; height:auto;">
+<p><a href="https://ffdecks.com/deck/5437075475136512">[FFDecks]</a></p>
+<h2 id="mono-elementals" tabindex="-1"><a class="header-anchor" href="#mono-elementals" aria-hidden="true">#</a> Mono Elementals</h2>
+<h3 id="mono-earth" tabindex="-1"><a class="header-anchor" href="#mono-earth" aria-hidden="true">#</a> Mono Earth</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/21-064L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images-qas/9-068H_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/10-068C_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/107.jpg" style="max-width:20%; height:auto;">
-<h2 id="mono-fire" tabindex="-1"><a class="header-anchor" href="#mono-fire" aria-hidden="true">#</a> Mono Fire</h2>
+<p><a href="https://ffdecks.com/deck/6362341252792320">[FFDecks]</a></p>
+<h3 id="mono-fire" tabindex="-1"><a class="header-anchor" href="#mono-fire" aria-hidden="true">#</a> Mono Fire</h3>
 <img src="https://storage.googleapis.com/materiahunter-prod.appspot.com/images/cards/PR/PR-153_12-002H_F_FA.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/22-113L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/22-009H_eg.jpg" style="max-width:20%; height:auto;">
-<h2 id="mono-ice" tabindex="-1"><a class="header-anchor" href="#mono-ice" aria-hidden="true">#</a> Mono Ice</h2>
+<img src="https://storage.googleapis.com/ffdecks-card-images/12-017H_eg.jpg" style="max-width:20%; height:auto;">
+<p><a href="https://ffdecks.com/deck/5100148192444416">[FFDecks]</a></p>
+<h3 id="mono-ice" tabindex="-1"><a class="header-anchor" href="#mono-ice" aria-hidden="true">#</a> Mono Ice</h3>
 <img src="https://storage.googleapis.com/materiahunter-prod.appspot.com/images/cards/22/22-032L_FA.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/20-040L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/16-042R_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/materiahunter-prod.appspot.com/images/cards/21/21-028H_F_FA.jpg" style="max-width:20%; height:auto;">
-<h2 id="mono-lightning" tabindex="-1"><a class="header-anchor" href="#mono-lightning" aria-hidden="true">#</a> Mono Lightning</h2>
+<p><a href="https://ffdecks.com/deck/6219195562852352">[FFDecks]</a></p>
+<h3 id="mono-lightning" tabindex="-1"><a class="header-anchor" href="#mono-lightning" aria-hidden="true">#</a> Mono Lightning</h3>
 <img src="https://storage.googleapis.com/materiahunter-prod.appspot.com/images/cards/21/21-081L_F_FA.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/22-120H_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/21-084H_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/materiahunter-prod.appspot.com/images/cards/22/22-090H_FA.jpg" style="max-width:20%; height:auto;">
-<h2 id="mono-water" tabindex="-1"><a class="header-anchor" href="#mono-water" aria-hidden="true">#</a> Mono Water</h2>
+<p><a href="https://ffdecks.com/deck/4960177104093184">[FFDecks]</a></p>
+<h3 id="mono-water" tabindex="-1"><a class="header-anchor" href="#mono-water" aria-hidden="true">#</a> Mono Water</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/22-122L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images-qas/9-114C_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/16-117H_eg.jpg" style="max-width:20%; height:auto;">
-<h2 id="mono-wind" tabindex="-1"><a class="header-anchor" href="#mono-wind" aria-hidden="true">#</a> Mono Wind</h2>
+<p><a href="https://ffdecks.com/deck/6303926207905792">[FFDecks]</a></p>
+<h3 id="mono-wind" tabindex="-1"><a class="header-anchor" href="#mono-wind" aria-hidden="true">#</a> Mono Wind</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/17-063R_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/10-055H_eg.jpg" style="max-width:20%; height:auto;">
-<img src="https://storage.googleapis.com/ffdecks-card-images/16-135S_eg.jpg" style="max-width:20%; height:auto;"></div></template>
+<img src="https://storage.googleapis.com/ffdecks-card-images/16-135S_eg.jpg" style="max-width:20%; height:auto;">
+<p><a href="https://ffdecks.com/deck/5106941488529408">[FFDecks]</a></p>
+</div></template>
 
 
