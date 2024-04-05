33ffa62da5f270ceaf93c1e8ff0305f95ccb52a7
kf <kevtfung@gmail.com>
2024-04-04 23:57:09 -0700

    update

--- docs/.vuepress/.temp/pages/blog/2024-04-30_Opus22_Decks.html.vue
+++ docs/.vuepress/.temp/pages/blog/2024-04-30_Opus22_Decks.html.vue
@@ -3,13 +3,20 @@
 <p>For deckbuilding advice <a href="2024-04-30_DeckbuildingTheory">click here</a></p>
 <h2 id="tier-list" tabindex="-1"><a class="header-anchor" href="#tier-list" aria-hidden="true">#</a> Tier List</h2>
 <h4 id="tier-1" tabindex="-1"><a class="header-anchor" href="#tier-1" aria-hidden="true">#</a> Tier 1</h4>
+<ul>
+<li>VIII Dredge, Wol 7, Warrior of Light</li>
+</ul>
 <h4 id="tier-2" tabindex="-1"><a class="header-anchor" href="#tier-2" aria-hidden="true">#</a> Tier 2</h4>
+<ul>
+<li>Ice, Wind/Lightning</li>
+</ul>
 <h4 id="tier-3" tabindex="-1"><a class="header-anchor" href="#tier-3" aria-hidden="true">#</a> Tier 3</h4>
 <h2 id="recent-events" tabindex="-1"><a class="header-anchor" href="#recent-events" aria-hidden="true">#</a> Recent Events</h2>
 <p><a href="https://ffdecks.com/tournament/2nd-shanghai-mengshu-cup/5090503440728064">(3/8) - 2nd-shanghai-mengshu-cup</a></p>
 <p><a href="https://ffdecks.com/tournament/fifth-annual-meijinsen/6255390711873536">(2/23) - fifth-annual-meijinsen</a></p>
 <p><a href="https://ffdecks.com/tournament/king-kong-ff-vii-rebirth-3v3-team-tournament/6086077010935808">(2/17) - king-kong-ff-vii-rebirth-3v3-team-tournament</a></p>
 <p><a href="https://ffdecks.com/tournament/winter-cup-north-carolina-day-2-singles/6301328356671488">(1/20) - winter-cup-north-carolina-day-2-singles</a></p>
+<p><a href="https://ffdecks.com/tournament/winter-cup-cardiff-2023/6286109945364480">(12/15/23) - winter-cup-cardiff-2023</a></p>
 <p><a href="https://ffdecks.com/tournament/fftcg-world-championships-2023-top-6/6197759347523584">(12/1/23) - fftcg-world-championships-2023-top-6</a></p>
 <h2 id="tribals" tabindex="-1"><a class="header-anchor" href="#tribals" aria-hidden="true">#</a> Tribals</h2>
 <h3 id="wol-7" tabindex="-1"><a class="header-anchor" href="#wol-7" aria-hidden="true">#</a> Wol 7</h3>
@@ -22,27 +29,56 @@
 <li><strong>Win Con</strong>: Flood the field using WoL 7 ETB. Dull/freeze or remove forwards.</li>
 <li><strong>Key Cards</strong>: Warrior of Light, Gilgamesh, Refia</li>
 <li><strong>Weaknesses</strong>: Flood the field using WoL 7 ETB. Dull/freeze or remove forwards.</li>
+<li><strong>More Info</strong>:</li>
 </ul>
-<h2 id="warriors" tabindex="-1"><a class="header-anchor" href="#warriors" aria-hidden="true">#</a> Warriors</h2>
+<h3 id="warriors" tabindex="-1"><a class="header-anchor" href="#warriors" aria-hidden="true">#</a> Warriors</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/21-010H_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/18-130L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/13-125R_eg.jpg" style="max-width:20%; height:auto;">
-<p><a href="">[FFDecks]</a></p>
+<p><a href="https://ffdecks.com/deck/4756632195039232">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>: Control</li>
+<li><strong>Win Con</strong>: Control the board with cancel summons, blanking abilities, and utilize Taivas for value and consistency.</li>
+<li><strong>Key Cards</strong>: Taivas, Amaterasu, Porom, Firion</li>
+<li><strong>Weaknesses</strong>: Explosive value plays, summon removal</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h3 id="knights" tabindex="-1"><a class="header-anchor" href="#knights" aria-hidden="true">#</a> Knights</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/12-103H_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/14-007L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/18-015R_eg.jpg" style="max-width:20%; height:auto;">
 <p><a href="https://ffdecks.com/deck/6210143231410176">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>: Aggro</li>
+<li><strong>Win Con</strong>: Flood the field with Knights. Search and play for reduced costs with Beatrix, Curilla, Ramza, and Gawain.</li>
+<li><strong>Key Cards</strong>: Beatrix, Curilla, Ramza, Garland</li>
+<li><strong>Weaknesses</strong>: Lack of board wipes and removal in general. Not much protection or interaction on opponent's turn. Not easy to deal with large boards or recover quickly after board wiped.</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h3 id="warriors-of-light" tabindex="-1"><a class="header-anchor" href="#warriors-of-light" aria-hidden="true">#</a> Warriors of Light</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/19-102L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/19-128L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/18-012L_eg.jpg" style="max-width:20%; height:auto;">
 <p><a href="https://ffdecks.com/deck/6220250161872896">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>: Mid Range/Control</li>
+<li><strong>Win Con</strong>: Set up backups to effiently cast Warrior of Light and maximize Refia's abilities.</li>
+<li><strong>Key Cards</strong>: Warrior of Light, Refia, Faris</li>
+<li><strong>Weaknesses</strong>: Cancel and blank, continous removal of 3 and 4 cost forwards especially on their turn.</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h3 id="scions" tabindex="-1"><a class="header-anchor" href="#scions" aria-hidden="true">#</a> Scions</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/16-100L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/20-088L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/20-086H_eg.jpg" style="max-width:20%; height:auto;">
-<p><a href="">[FFDecks]</a></p>
+<p><a href="https://ffdecks.com/deck/6377231619194880">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>: Mid Range</li>
+<li><strong>Win Con</strong>: Remove forwards with abilities, summons, and EX. Incremental value cards with Alisaie, G'raha Tia, and Estinien. Forwards with haste to get in surprise damage.</li>
+<li><strong>Key Cards</strong>: Y'shtola, Alisaie, Odin</li>
+<li><strong>Weaknesses</strong>: aggro, efficient bounce/removal, decks with explosive plays/turns and can rebuild quickly</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h2 id="categories" tabindex="-1"><a class="header-anchor" href="#categories" aria-hidden="true">#</a> Categories</h2>
 <h3 id="vi" tabindex="-1"><a class="header-anchor" href="#vi" aria-hidden="true">#</a> VI</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/15-042R_eg.jpg" style="max-width:20%; height:auto;">
@@ -50,44 +86,100 @@
 <img src="https://storage.googleapis.com/ffdecks-card-images/17-017H_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/20-007L_eg.jpg" style="max-width:20%; height:auto;">
 <p><a href="">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>: Aggro</li>
+<li><strong>Win Con</strong>: Get Locke and draw excessively by casting cards at reduced costs such as Sabin and Mog VI. Edgar/Sabin gets a free kill each turn. Demon can board wipe and remove the break zone.</li>
+<li><strong>Key Cards</strong>: Locke, Edgar, Sabin, Demon</li>
+<li><strong>Weaknesses</strong>: Limited to only VI cards in order to maximize Locke. If you can keep Locke off the field, must play a fair game.</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h3 id="viii" tabindex="-1"><a class="header-anchor" href="#viii" aria-hidden="true">#</a> VIII</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/22-079L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/22-031H_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/21-081L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/22-010L_eg.jpg" style="max-width:20%; height:auto;">
-<p><a href="">[FFDecks]</a></p>
+<p><a href="https://ffdecks.com/deck/6503793098227712">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>: Aggro</li>
+<li><strong>Win Con</strong>: Don Corneo mill plays Fujin and Raijin for free. Gilgamesh follow up to OTK.</li>
+<li><strong>Key Cards</strong>: Don Corneo, Rughadjeen, Gilgamesh</li>
+<li><strong>Weaknesses</strong>: Mist dragon, summons/EX removal</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h3 id="xiii" tabindex="-1"><a class="header-anchor" href="#xiii" aria-hidden="true">#</a> XIII</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/19-138S_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/materiahunter-prod.appspot.com/images/cards/PR/PR-146_19-129S_NF_FA.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/19-137S_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/19-130S_eg.jpg" style="max-width:20%; height:auto;">
 <p><a href="https://ffdecks.com/deck/6227716375314432">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>: Aggro</li>
+<li><strong>Win Con</strong>: Attack with 3 XII forwards and trigger Lightning's draw, buff, and damage abilities.</li>
+<li><strong>Key Cards</strong>: Lightning, Vanille, Hope</li>
+<li><strong>Weaknesses</strong>: blank, summon removal, amaterasu</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h3 id="xiv" tabindex="-1"><a class="header-anchor" href="#xiv" aria-hidden="true">#</a> XIV</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/17-016L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/19-124L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/14-015R_eg.jpg" style="max-width:20%; height:auto;">
 <p><a href="https://ffdecks.com/deck/6275752363294720">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>: Aggro</li>
+<li><strong>Win Con</strong>: Get 3-4 forwards with haste and play Hien to attack twice a turn for OTK. Utilize Scion synergies with other XIV forwards</li>
+<li><strong>Key Cards</strong>: Hien, Estinien, Alisaie, Y'shtola</li>
+<li><strong>Weaknesses</strong>: Amaterasu, blank, Summon/EX removal. Board wipes semi effective if you can survive</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h2 id="dual-elementals" tabindex="-1"><a class="header-anchor" href="#dual-elementals" aria-hidden="true">#</a> Dual Elementals</h2>
 <h3 id="ice-water" tabindex="-1"><a class="header-anchor" href="#ice-water" aria-hidden="true">#</a> Ice/Water</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/21-027L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/17-138S_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/21-122H_eg.jpg" style="max-width:20%; height:auto;">
 <p><a href="https://ffdecks.com/deck/6427954394431488">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>:</li>
+<li><strong>Win Con</strong>:</li>
+<li><strong>Key Cards</strong>:</li>
+<li><strong>Weaknesses</strong>:</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h3 id="wind-lightning" tabindex="-1"><a class="header-anchor" href="#wind-lightning" aria-hidden="true">#</a> Wind/Lightning</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/19-114L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/17-096H_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/16-135S_eg.jpg" style="max-width:20%; height:auto;">
 <p><a href="https://ffdecks.com/deck/6305146179944448">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>:</li>
+<li><strong>Win Con</strong>:</li>
+<li><strong>Key Cards</strong>:</li>
+<li><strong>Weaknesses</strong>:</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h3 id="wind-water" tabindex="-1"><a class="header-anchor" href="#wind-water" aria-hidden="true">#</a> Wind/Water</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/11-124H_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/21-133S_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/19-118L_eg.jpg" style="max-width:20%; height:auto;">
 <p><a href="https://ffdecks.com/deck/6275657387474944">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>:</li>
+<li><strong>Win Con</strong>:</li>
+<li><strong>Key Cards</strong>:</li>
+<li><strong>Weaknesses</strong>:</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h3 id="earth-water" tabindex="-1"><a class="header-anchor" href="#earth-water" aria-hidden="true">#</a> Earth/Water</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/19-119L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/16-129L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/19-105H_eg.jpg" style="max-width:20%; height:auto;">
 <p><a href="https://ffdecks.com/deck/5437075475136512">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>:</li>
+<li><strong>Win Con</strong>:</li>
+<li><strong>Key Cards</strong>:</li>
+<li><strong>Weaknesses</strong>:</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h2 id="mono-elementals" tabindex="-1"><a class="header-anchor" href="#mono-elementals" aria-hidden="true">#</a> Mono Elementals</h2>
 <h3 id="mono-earth" tabindex="-1"><a class="header-anchor" href="#mono-earth" aria-hidden="true">#</a> Mono Earth</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/21-064L_eg.jpg" style="max-width:20%; height:auto;">
@@ -95,34 +187,76 @@
 <img src="https://storage.googleapis.com/ffdecks-card-images/10-068C_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/107.jpg" style="max-width:20%; height:auto;">
 <p><a href="https://ffdecks.com/deck/6362341252792320">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>:</li>
+<li><strong>Win Con</strong>:</li>
+<li><strong>Key Cards</strong>:</li>
+<li><strong>Weaknesses</strong>:</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h3 id="mono-fire" tabindex="-1"><a class="header-anchor" href="#mono-fire" aria-hidden="true">#</a> Mono Fire</h3>
 <img src="https://storage.googleapis.com/materiahunter-prod.appspot.com/images/cards/PR/PR-153_12-002H_F_FA.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/22-113L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/22-009H_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/12-017H_eg.jpg" style="max-width:20%; height:auto;">
 <p><a href="https://ffdecks.com/deck/5100148192444416">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>:</li>
+<li><strong>Win Con</strong>:</li>
+<li><strong>Key Cards</strong>:</li>
+<li><strong>Weaknesses</strong>:</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h3 id="mono-ice" tabindex="-1"><a class="header-anchor" href="#mono-ice" aria-hidden="true">#</a> Mono Ice</h3>
 <img src="https://storage.googleapis.com/materiahunter-prod.appspot.com/images/cards/22/22-032L_FA.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/20-040L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/16-042R_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/materiahunter-prod.appspot.com/images/cards/21/21-028H_F_FA.jpg" style="max-width:20%; height:auto;">
 <p><a href="https://ffdecks.com/deck/6219195562852352">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>:</li>
+<li><strong>Win Con</strong>:</li>
+<li><strong>Key Cards</strong>:</li>
+<li><strong>Weaknesses</strong>:</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h3 id="mono-lightning" tabindex="-1"><a class="header-anchor" href="#mono-lightning" aria-hidden="true">#</a> Mono Lightning</h3>
 <img src="https://storage.googleapis.com/materiahunter-prod.appspot.com/images/cards/21/21-081L_F_FA.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/22-120H_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/21-084H_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/materiahunter-prod.appspot.com/images/cards/22/22-090H_FA.jpg" style="max-width:20%; height:auto;">
 <p><a href="https://ffdecks.com/deck/4960177104093184">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>:</li>
+<li><strong>Win Con</strong>:</li>
+<li><strong>Key Cards</strong>:</li>
+<li><strong>Weaknesses</strong>:</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h3 id="mono-water" tabindex="-1"><a class="header-anchor" href="#mono-water" aria-hidden="true">#</a> Mono Water</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/22-122L_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images-qas/9-114C_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/16-117H_eg.jpg" style="max-width:20%; height:auto;">
 <p><a href="https://ffdecks.com/deck/6303926207905792">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>:</li>
+<li><strong>Win Con</strong>:</li>
+<li><strong>Key Cards</strong>:</li>
+<li><strong>Weaknesses</strong>:</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 <h3 id="mono-wind" tabindex="-1"><a class="header-anchor" href="#mono-wind" aria-hidden="true">#</a> Mono Wind</h3>
 <img src="https://storage.googleapis.com/ffdecks-card-images/17-063R_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/10-055H_eg.jpg" style="max-width:20%; height:auto;">
 <img src="https://storage.googleapis.com/ffdecks-card-images/16-135S_eg.jpg" style="max-width:20%; height:auto;">
 <p><a href="https://ffdecks.com/deck/5106941488529408">[FFDecks]</a></p>
+<ul>
+<li><strong>Playstyle</strong>:</li>
+<li><strong>Win Con</strong>:</li>
+<li><strong>Key Cards</strong>:</li>
+<li><strong>Weaknesses</strong>:</li>
+<li><strong>More Info</strong>:</li>
+</ul>
 </div></template>
 
 
